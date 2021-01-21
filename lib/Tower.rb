af7512490eb2173245c9acf04288fa4356e12f35
Kenny Li <kenny.wj.li@gmail.com>
2021-01-21 16:32:27 -0500

    Started Tower of Hanoi

--- lib/Tower.rb
+++ lib/Tower.rb
@@ -1,3 +1,24 @@
 class Board 
-
+    attr_reader :board, :num_of_disc
+    def initialize
+        @board = Array.new(1){Array.new(5)}
+        @num_of_disc = 5
+        fill_board
+    end
+    def fill_board()
+        i = @num_of_disc
+        while i > 0
+        @board[0] << i
+            i -= 1
+        end
+    end
+    def won?
+        if @board[-1] ==[1..@num_of_disc].reverse!
+            print "you won"
+        end
+    end
+    def get_input
+        print "Enter 2 numbers (disc), and 1 number(row) Ex: 0 3, 2"
+        input = gets.chomp.to_a
+    end
 end
