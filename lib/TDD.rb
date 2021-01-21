b60c300e152f6423c832b263e4e47094d643e73c
Kenny Li <kenny.wj.li@gmail.com>
2021-01-21 14:54:02 -0500

    finished two_sum

--- lib/TDD.rb
+++ lib/TDD.rb
@@ -14,5 +14,16 @@ class Array
         array 
     end
 
-
+    def two_sum
+        arr = []
+        (0...self.length).each do |i|
+            ((i+1)...self.length).each do |j|
+                pairs = [i,j]
+                if self[i] + self[j] == 0
+                    arr << pairs
+                end
+            end
+        end
+        arr
+    end
 end
--- spec/TDD_spec.rb
+++ spec/TDD_spec.rb
@@ -2,6 +2,7 @@ require "rspec"
 require "TDD"
 
 describe Array do 
+    let (:two_sum_array){[-1, 0, 2, -2, 1]}
     describe '#uniq' do
         it "should return unique elements " do
             expect([1, 2, 1, 3, 3].uniq).to eq([1,2,3])
@@ -13,11 +14,16 @@ describe Array do
     end
 
     describe '#two_sum' do 
-        it "find all pairs of positions where elements sum to 0"
-
-        
-
+        it "find all pairs of positions where elements sum to 0" do
+            expect(two_sum_array.two_sum).to eq([[0,4], [2,3]])
+        end
+        it "smaller elements come first, smaller second elements come first" do
+            expect(two_sum_array.two_sum).to_not eq([[4,0], [3,2]])
+        end
     end
-
-
+    describe "#transpose" do
+        it "should convert between row oriented and col oriented 2D array" do
+            
+        end
+    end
 end
