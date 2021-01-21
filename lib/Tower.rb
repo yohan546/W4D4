43c76210ca2225771329b21156158761e0ca60ac
yohan oh <yohan.oh332@gmail.com>
2021-01-21 16:59:54 -0500

    finished first draft of Tower.rb

--- lib/Tower.rb
+++ lib/Tower.rb
@@ -1,7 +1,7 @@
 class Board 
     attr_reader :board, :num_of_disc
     def initialize
-        @board = Array.new(1){Array.new(5)}
+        @board = Array.new(3){Array.new}
         @num_of_disc = 5
         fill_board
     end
@@ -18,7 +18,35 @@ class Board
         end
     end
     def get_input
-        print "Enter 2 numbers (disc), and 1 number(row) Ex: 0 3, 2"
-        input = gets.chomp.to_a
+        print "Enter 2 numbers for a start and end position of disc, Ex. 0 2"
+        input = gets.chomp.to_a(" ") 
+        input.map!{ |num| num.to_i}
+
+        if !valid_move?(input)
+            raise "invalid move"
+            get_input
+        end
     end
+
+    def move
+        until won?
+            pos = get_input 
+            valid_move?(pos)
+
+            ele = @board.pop([pos[0]])
+            @board[pos[1]] << ele  
+            
+        end
+    end
+
+    def valid_move?(pos)
+        start_pos, end_pos = pos
+
+        if @board[end_pos].empty? || @board[end_pos][-1] > @board[start_pos][-1]
+            return true 
+        else
+            return false
+        end
+    end
+
 end
